f3f717dfe5fa47da49e8b262800a1d2d9cd0882f
LioKor <support@liokor.com>
2019-03-10 21:11:09 +0300

    helper_functions.js refactoring and README.md update

--- scripts/helper_functions.js
+++ scripts/helper_functions.js
@@ -3,8 +3,8 @@ function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function circleCollision(s1, s2) {
-    if (Math.pow(s2.x - s1.x, 2) + Math.pow(s2.y - s1.y, 2) <= Math.pow(s2.r + s1.r, 2)) {
+function circleCollision(c1, c2) {
+    if (Math.pow(c2.x - c1.x, 2) + Math.pow(c2.y - c1.y, 2) <= Math.pow(c2.r + c1.r, 2)) {
         return true;
     } else {
         return false;
